fb1839e57b5a0f610a3e6431babd5575ad5178e2
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2021-10-07 19:57:36 +0300

    oops

--- js/menu_API_to_html.js
+++ js/menu_API_to_html.js
@@ -1,13 +1,10 @@
 import {loadRestaurantMenu} from "./parse_restaurant_menu.js";
 
-
-
 (async function parseFetchedRestaurantMenu() {
     let menuArray = []
 
     await fetch('restaurants_menu/mcdonalds.json')
         .then(response => response.json())
         .then(data => menuArray.push(data))
-        .then(console.log(menuArray))
         loadRestaurantMenu(menuArray)
 })();
